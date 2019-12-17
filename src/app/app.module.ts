ca26d339430309e975b28c024ac0db3a33a47b5a
wevertoum <wevertonmixalt@gmail.com>
2019-12-17 11:26:33 -0300

    3 files modified, 3 added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
 import { RouterModule } from "@angular/router";
 import { ReactiveFormsModule } from "@angular/forms";
-
+import { HttpClientModule } from "@angular/common/http"
 import { AppComponent } from "./app.component";
 import { TopBarComponent } from "./top-bar/top-bar.component";
 import { ProductListComponent } from "./product-list/product-list.component";
@@ -10,10 +10,12 @@ import { ProductAlertsComponent } from "./product-alerts/product-alerts.componen
 import { ProductDetailsComponent } from "./product-details/product-details.component";
 import { CartService } from './cart.service';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   imports: [
     BrowserModule,
+    HttpClientModule,
     ReactiveFormsModule,
     RouterModule.forRoot([
       { path: "", component: ProductListComponent },
@@ -27,7 +29,8 @@ import { CartComponent } from './cart/cart.component';
     ProductListComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent
   ],
   bootstrap: [AppComponent],
   providers: [CartService]
